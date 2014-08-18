111b297fc78d739dd605e60874c3b288e99f762f
Guillaume <f.e.a.rcombat@hotmail.fr>
2014-08-18 15:34:34 -0400

    :neckbeard: Added .gitattributes & .gitignore files

---
+++ zBooba.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<link type='text/css' rel='stylesheet' href='zBooba.css'/>
+	
+		<script type='text/javascript' src='zBooba.js'></script>
+		<script type='text/javascript'>
+			
+		</script>
+	</head>
+	
+	<body onload='loadZBooba();'>
+		<textarea id='textarea'>This is a test.</textarea>
+	</body>
+</html>
